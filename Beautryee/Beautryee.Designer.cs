ec8d1718997e6c5bac12e0fdc6d0ef598d07c826
huangshikun <huangshikun2008@126.com>
2021-07-20 09:59:49 +0800

    添加项目文件。

---
+++ Beautryee/Beautryee.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Beautryee
+{
+    partial class Beautryee
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Beautryee));
+            this.Label_Score = new System.Windows.Forms.Label();
+            this.Button_Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Link_LevelDown = new System.Windows.Forms.LinkLabel();
+            this.Link_LevelUp = new System.Windows.Forms.LinkLabel();
+            this.Label_Level = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Label_Score
+            // 
+            this.Label_Score.AutoSize = true;
+            this.Label_Score.BackColor = System.Drawing.Color.Transparent;
+            this.Label_Score.Font = new System.Drawing.Font("宋体", 12F);
+            this.Label_Score.ForeColor = System.Drawing.Color.Black;
+            this.Label_Score.Location = new System.Drawing.Point(415, 35);
+            this.Label_Score.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label_Score.Name = "Label_Score";
+            this.Label_Score.Size = new System.Drawing.Size(72, 16);
+            this.Label_Score.TabIndex = 0;
+            this.Label_Score.Text = "Score: 0";
+            // 
+            // Button_Start
+            // 
+            this.Button_Start.Location = new System.Drawing.Point(451, 338);
+            this.Button_Start.Name = "Button_Start";
+            this.Button_Start.Size = new System.Drawing.Size(75, 23);
+            this.Button_Start.TabIndex = 1;
+            this.Button_Start.Text = "开始游戏";
+            this.Button_Start.UseVisualStyleBackColor = true;
+            this.Button_Start.Click += new System.EventHandler(this.Button_Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(415, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Level";
+            // 
+            // Link_LevelDown
+            // 
+            this.Link_LevelDown.AutoSize = true;
+            this.Link_LevelDown.Font = new System.Drawing.Font("宋体", 12F);
+            this.Link_LevelDown.ForeColor = System.Drawing.Color.Black;
+            this.Link_LevelDown.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.Link_LevelDown.LinkColor = System.Drawing.Color.Blue;
+            this.Link_LevelDown.Location = new System.Drawing.Point(472, 82);
+            this.Link_LevelDown.Name = "Link_LevelDown";
+            this.Link_LevelDown.Size = new System.Drawing.Size(16, 16);
+            this.Link_LevelDown.TabIndex = 3;
+            this.Link_LevelDown.TabStop = true;
+            this.Link_LevelDown.Text = "-";
+            this.Link_LevelDown.VisitedLinkColor = System.Drawing.Color.Black;
+            this.Link_LevelDown.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Link_LevelDown_LinkClicked);
+            // 
+            // Link_LevelUp
+            // 
+            this.Link_LevelUp.AutoSize = true;
+            this.Link_LevelUp.Font = new System.Drawing.Font("宋体", 12F);
+            this.Link_LevelUp.ForeColor = System.Drawing.Color.Black;
+            this.Link_LevelUp.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.Link_LevelUp.LinkColor = System.Drawing.Color.Blue;
+            this.Link_LevelUp.Location = new System.Drawing.Point(527, 82);
+            this.Link_LevelUp.Name = "Link_LevelUp";
+            this.Link_LevelUp.Size = new System.Drawing.Size(16, 16);
+            this.Link_LevelUp.TabIndex = 4;
+            this.Link_LevelUp.TabStop = true;
+            this.Link_LevelUp.Text = "+";
+            this.Link_LevelUp.VisitedLinkColor = System.Drawing.Color.Black;
+            this.Link_LevelUp.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Link_LevelUp_LinkClicked);
+            // 
+            // Label_Level
+            // 
+            this.Label_Level.AutoSize = true;
+            this.Label_Level.Location = new System.Drawing.Point(499, 84);
+            this.Label_Level.Name = "Label_Level";
+            this.Label_Level.Size = new System.Drawing.Size(11, 12);
+            this.Label_Level.TabIndex = 5;
+            this.Label_Level.Text = "1";
+            this.Label_Level.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Beautryee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(564, 400);
+            this.Controls.Add(this.Label_Level);
+            this.Controls.Add(this.Link_LevelUp);
+            this.Controls.Add(this.Link_LevelDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Button_Start);
+            this.Controls.Add(this.Label_Score);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Beautryee";
+            this.Text = "贪吃蛇";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Beautryee_FormClosing);
+            this.Load += new System.EventHandler(this.Beautryee_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_Score;
+        private System.Windows.Forms.Button Button_Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel Link_LevelDown;
+        private System.Windows.Forms.LinkLabel Link_LevelUp;
+        private System.Windows.Forms.Label Label_Level;
+    }
+}
+
---
+++ Beautryee/Beautryee.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Beautryee
+{
+    public enum Direction
+    {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT
+    };
+    public partial class Beautryee : Form
+    {
+        [DllImport("user32.dll")] //申明API函数
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, Keys vk);
+        [DllImport("user32.dll")] //申明API函数
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        private Panel[,] MapItems;          // 地图集合
+        private readonly int Row = 20;      // 格子行数
+        private readonly int Col = 20;      // 格子列数
+        private readonly int ItemSize = 20; // 格子边长
+        private double Interval = 200;      // 移动速度
+        private Random ran;                 // 随机生成苹果
+        private Point Apple = new Point(0, 0);  // 苹果位置
+        private Point SnakeHeader;          // 蛇头位置
+        private int SnakeLenth = 3;         // 蛇身长度
+        private int Score = 0;              // 分数
+        private int Level = 1;              // 等级
+        private bool GameOver = false;      // 游戏是否结束
+        private bool IsRelease = false;     // 上一个按键是否已经执行
+        private readonly Color MapColor = SystemColors.Info;    // 地图颜色
+        private readonly Color SnakeColor = Color.SaddleBrown;  // 蛇颜色
+        private Direction direction = Direction.RIGHT;          // 移动方向
+        private readonly Queue<Point> SnakeBody = new Queue<Point> { }; // 蛇身集合
+
+        public Beautryee()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Beautryee_Load(object sender, EventArgs e)
+        {
+            RegisterHotKey();
+            ran = new Random((int)DateTime.Now.Ticks);
+            GenerateMap();
+        }
+        // 开始游戏
+        private void Button_Start_Click(object sender, EventArgs e)
+        {
+            NewGame();
+            Button_Start.Enabled = false;
+        }
+        // 新游戏
+        private void NewGame()
+        {
+            foreach (var item in SnakeBody)
+                MapItems[item.X, item.Y].BackColor = MapColor;
+            MapItems[Apple.X, Apple.Y].BackColor = MapColor;
+
+            GameOver = false;
+            Score = 0;
+            Level = int.Parse(Label_Level.Text);
+            Interval *= Math.Pow(0.9, (double)Level - 1);
+            SnakeLenth = 3;
+            direction = Direction.RIGHT;
+
+            Label_Score.Text = $"Score: {Score}";
+
+            GenerateSnake();
+            GenerateApple();
+            new Thread(new ThreadStart(Running)).Start();
+        }
+        // 注册快捷键
+        private void RegisterHotKey()
+        {
+            try
+            {
+                RegisterHotKey(Handle, (int)Direction.UP, 0, Keys.Up);
+                RegisterHotKey(Handle, (int)Direction.RIGHT, 0, Keys.Right);
+                RegisterHotKey(Handle, (int)Direction.DOWN, 0, Keys.Down);
+                RegisterHotKey(Handle, (int)Direction.LEFT, 0, Keys.Left);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("注册快捷键失败\n" + ex.Message);
+            }
+        }
+        // 生成地图
+        private void GenerateMap()
+        {
+            MapItems = new Panel[Col, Row];
+            for (int x = 0; x < Col; x++)
+            {
+                for (int y = 0; y < Row; y++)
+                {
+                    Panel item = new Panel
+                    {
+                        Name = $"Box{x}_{y}",
+                        Size = new Size(ItemSize, ItemSize),
+                        Location = new Point(x * ItemSize, y * ItemSize),
+                        BackColor = MapColor,
+                        Enabled = false,
+                    };
+                    MapItems[x,y] = item;
+                    Controls.Add(item);
+                }
+            }
+        }
+        // 生成蛇
+        private void GenerateSnake()
+        {
+            SnakeBody.Clear();
+            for (int i = 0; i < SnakeLenth; i++)
+            {
+                SnakeBody.Enqueue(new Point(i, 0));
+                MapItems[i, 0].BackColor = SnakeColor;
+            }
+
+            SnakeHeader = new Point(SnakeLenth-1, 0);
+        }
+        // 生成苹果
+        private void GenerateApple()
+        {
+            while (true)
+            {
+                int x = ran.Next(0, Col);
+                int y = ran.Next(0, Row);
+                Apple = new Point(x, y);
+                if (!SnakeBody.Contains(Apple))
+                {
+                    MapItems[x, y].BackColor = Color.Red;
+                    break;
+                }
+            }
+        }
+        // 移动
+        private void Running()
+        {
+            while (!GameOver)
+            {
+                Thread.Sleep((int)Interval);
+                IsRelease = true;
+                try
+                {
+                    StartPosition:
+                    if (direction == Direction.RIGHT)
+                    {
+                        SnakeHeader.Offset(1, 0);
+                    }
+                    else if (direction == Direction.DOWN)
+                    {
+                        SnakeHeader.Offset(0, 1);
+                    }
+                    else if (direction == Direction.LEFT)
+                    {
+                        SnakeHeader.Offset(-1, 0);
+                    }
+                    else if (direction == Direction.UP)
+                    {
+                        SnakeHeader.Offset(0, -1);
+                    }
+                    if (SnakeBody.Contains(SnakeHeader))
+                    {
+                        GameOver = true;
+                    }
+                    MapItems[SnakeHeader.X, SnakeHeader.Y].BackColor = SnakeColor;
+                    if (SnakeHeader == Apple)
+                    {
+                        SnakeBody.Enqueue(SnakeHeader);
+                        Score++;
+                        SnakeLenth++;
+                        GenerateApple();
+                        Label_Score.Text = $"Score: {Score}";
+                        goto StartPosition;
+                    }
+                    else
+                    {
+                        SnakeBody.Enqueue(SnakeHeader);
+                        Point item = SnakeBody.Dequeue();
+                        MapItems[item.X, item.Y].BackColor = MapColor;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    GameOver = true;
+                }
+            }
+            MessageBox.Show($"游戏结束，分数:{Score}", "游戏结束");
+            Button_Start.Enabled = true;
+        }
+        // 上
+        private void HotKey_Up()
+        {
+            if (!IsRelease || direction != Direction.LEFT && direction != Direction.RIGHT)
+                return;
+            direction = Direction.UP;
+            IsRelease = false;
+        }
+        // 右
+        private void HotKey_Right()
+        {
+            if (!IsRelease || direction != Direction.UP && direction != Direction.DOWN)
+                return;
+            direction = Direction.RIGHT;
+            IsRelease = false;
+        }
+        // 下
+        private void HotKey_Down()
+        {
+            if (!IsRelease || direction != Direction.LEFT && direction != Direction.RIGHT)
+                return;
+            direction = Direction.DOWN;
+            IsRelease = false;
+        }
+        // 左
+        private void HotKey_Left()
+        {
+            if (!IsRelease || direction != Direction.UP && direction != Direction.DOWN)
+                return;
+            direction = Direction.LEFT;
+            IsRelease = false;
+        }
+        // 按下设定的键时调用该函数
+        private void ProcessHotkey(Message m)
+        {
+            int id = m.WParam.ToInt32();
+            switch (id)
+            {
+                case (int)Direction.UP:     HotKey_Up(); break;
+                case (int)Direction.RIGHT:  HotKey_Right(); break;
+                case (int)Direction.DOWN:   HotKey_Down(); break;
+                case (int)Direction.LEFT:   HotKey_Left(); break;
+            }
+        }
+        //监视Windows消息
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:ProcessHotkey(m); break;
+            }
+            base.WndProc(ref m);
+        }
+        // 关闭窗体
+        private void Beautryee_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        // 等级加
+        private void Link_LevelUp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (Level < 10)
+                Level++;
+
+            Label_Level.Text = Level.ToString();
+        }
+        // 等级减
+        private void Link_LevelDown_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (Level > 1)
+                Level--;
+
+            Label_Level.Text = Level.ToString();
+        }
+    }
+}
